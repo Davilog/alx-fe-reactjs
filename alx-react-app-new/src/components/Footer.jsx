468624744d8b50282b4d9671e3c3c945a2790de3
David Adedeji <davidmayowa8@gmail.com>
2025-09-07 16:09:47 +0100

    made updates

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer
+    style={{
+        backgroundColor: '#222',
+        color: 'white',
+        padding: '15px',
+        textAlign: 'center',
+        marginTop: '20px',
+      }}
+    >
+      <p style={{ fontSize: '14px' }}>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -0,0 +1,26 @@
+const UserProfile = (props) => {
+  return (
+    <div
+    style={{
+        border: '1px solid gray',
+        padding: '10px',
+        margin: '10px',
+        borderRadius: '8px',
+        backgroundColor: '#f9f9f9',
+      }}
+      >
+      <h2 style={{ color: 'blue', fontSize: '20px', marginBottom: '8px' }}>
+        {props.name}
+        </h2>
+      <p>
+        Age:{' '}
+        <span style={{ fontWeight: 'bold', color: 'darkred' }}>
+         {props.age}
+         </span>
+         </p>
+      <p style={{ fontStyle: 'italic', color: '#333' }}>Bio: {props.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
