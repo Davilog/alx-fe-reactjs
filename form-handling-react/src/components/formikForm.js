dd62a9203834253d01cbf154a58e0e71112c6fc6
David Adedeji <davidmayowa8@gmail.com>
2025-10-12 17:01:48 +0100

    adjusted the yup validation schema

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -2,14 +2,9 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 const validationSchema = Yup.object({
-  username: Yup.string()
-    .required('Username is required'),
-  email: Yup.string()
-    .email('Email is invalid')
-    .required('Email is required'),
-  password: Yup.string()
-    .min(6, 'Password must be at least 6 characters')
-    .required('Password is required')
+  username: Yup.string().required('Username is required'),
+  email: Yup.string().required('Email is required'),
+  password: Yup.string().required('Password is required')
 });
 
 const FormikForm = () => {
