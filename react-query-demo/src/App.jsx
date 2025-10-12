5171332fc056d11c655b31319730cccf0611df3d
David Adedeji <davidmayowa8@gmail.com>
2025-10-12 17:17:59 +0100

    fixed the setup in the main component

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Navigation from './components/Navigation';
+import PostsComponent from './components/PostsComponent';
 import './App.css';
 
 const queryClient = new QueryClient();
@@ -7,9 +7,7 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div style={{ minHeight: '100vh', backgroundColor: '#f5f5f5' }}>
-        <Navigation />
-      </div>
+      <PostsComponent />
     </QueryClientProvider>
   );
 }
